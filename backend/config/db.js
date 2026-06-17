bcc2d68de9e4927cf26bdeacd387fb4eabb6080f
bibek Tamang <bibeklama123asd@gmail.com>
2026-06-17 22:22:56 +0545

    Fix MongoDB connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const mongoDb = async () => {
   try {
+    console.log("MONGO_URL =", process.env.MONGO_URL);
     await mongoose.connect(process.env.MONGO_URL);
     console.log("MongoDB is connected");
   } catch (error) {
