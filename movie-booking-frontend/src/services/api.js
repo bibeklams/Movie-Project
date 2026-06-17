082354afe83a52331d2fe8f177cc0844f402a988
bibek Tamang <bibeklama123asd@gmail.com>
2026-06-17 22:35:19 +0545

    Prepare frontend for deployment

--- movie-booking-frontend/src/services/api.js
+++ movie-booking-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: "https://movie-project-zuez.onrender.com/api",
   withCredentials: true,
 });
 
